9740df01e27dfd1474243ae20917db983f52e1f7
Achiillless <pavlenkomark06@gmail.com>
2022-12-20 13:58:17 +0200

    slider dev

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,19 +3,8 @@ $(document).ready(function(){
     $('select').niceSelect();
 })
 
-let mybutton = document.getElementById("myBtn");
+$(document).ready(function(){
 
-window.onscroll = function() {scrollFunction()};
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-  }
+  $('.single-item').slick();
+    
+});
